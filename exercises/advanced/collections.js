065522445f1b55ec2173e84268a7fe44df5cf86e
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-09-29 11:40:09 -0400

    Completes Promise.all prompt

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -8,9 +8,43 @@
  *    4. Writes the new file to the file located at `writePath`
  */
 
+let Promise = require('bluebird');
+let fs = require('fs');
 
 var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
+  let pluckFirstLineFromFilePromise = function(filePath) {
+    return new Promise(function(resolve, reject) {
+      fs.readFile(filePath, 'utf8', function(err, data) {
+        if (err) {
+          reject(err);
+        }
+        resolve(data.split('\n')[0]);
+      });
+    });
+  };
+
+  let promisesOfFirstLinesFromEachFile = filePaths.map(function(filePath) {
+    return pluckFirstLineFromFilePromise(filePath);
+  });
+
+  return Promise.all(promisesOfFirstLinesFromEachFile)
+    .then(function(firstLinesOfEachFile) {
+      firstLinesOfEachFile = firstLinesOfEachFile.reduce(function(acc, currentValue) {
+        return acc + currentValue + '\n';
+      }, '');
+
+      firstLinesOfEachFile = firstLinesOfEachFile.slice(0, -1);
+
+      console.log(firstLinesOfEachFile);
+      fs.writeFile(writePath, firstLinesOfEachFile, function(err) {
+        if (err) {
+          return err;
+        }
+      });
+    })
+    .catch(function(err) {
+      throw err;
+    });
 };
 
 // Export these functions so we can unit test them
